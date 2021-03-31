a91c72a054414d454276ec3dc550dd131fdbd3c3
tomekaq <tomekaq@wp.pl>
2021-03-31 16:24:38 +0200

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#cd ~/Pulpit/corsika-77100/src/utils/coast/CorsikaRead # command to run Corsika simulation
+#inpCorsika=$(./CorsikaReader DAT000001.inclined)
+
+cd ~/Pulpit
+#touch outputCorsikaInclined.txt
+#echo "$inpCorsika" > outputCorsikaInclined.txt
+i=0
+input=$1
+patternid='[0-9]+'
+patternX='x=([-+][0-9]+\.?[0-9]*)|x=([0-9]*\.[0-9]+)|x=([0-9]+)'
+patternY='y=([-+][0-9]+\.?[0-9]*)|y=([0-9]*\.[0-9]+)|y=([0-9]+)'
+patternE='e=*.[0-9]{0,7}.[0-9]{0,3}*'
+patternW='w=([-+][0-9]+\.?[0-9]*)|w=([0-9]*\.[0-9]+)|w=([0-9]+)'
+
+> filtered.txt
+while IFS= read -r line
+do  
+[[ $line =~ $patternid ]]
+a=${BASH_REMATCH[0]}
+j="10000.0"
+k="-10000.0"
+if [[ ${BASH_REMATCH[0]} -eq $2 ]]
+then
+   ((i = i+1)) 
+    echo $line >> filtered.txt
+fi
+done < "$input" 
+echo "$i"
