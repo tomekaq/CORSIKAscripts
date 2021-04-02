10ef296cb91f105cf47df0aff0312d7b01df6168
tomekaq <tomekaq@wp.pl>
2021-04-02 21:12:38 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,39 +1,69 @@
 #!/bin/bash
+# Tomasz Antonik
 
-#cd ~/Pulpit/corsika-77100/src/utils/coast/CorsikaRead # command to run Corsika simulation
-#inpCorsika=$(./CorsikaReader DAT000001.inclined)
+#  Program wczytuje dane odczytane przez program ./CorsikaRead i zapisuje je do pliku tekstowego.
+#  W dalszej częsci będą one filtrowane. 
+#  Następnie program rozdziela cząstki do mniejszego pliku zgodnie z podanymi przez użytkownika
+#  parametrami położenia X i Y 
 
-cd ~/Pulpit
-#touch outputCorsikaInclined.txt
-#echo "$inpCorsika" > outputCorsikaInclined.txt
+# Program reads data from ./CorsikaRead and writes it to new file.
+# Then program reads .txt file and divines particle data to another, smaller .txt file.
+#  according to the parameters given by the user
+
+echo "Enter name of file to read in ./CorsikaRead"
+read binaryInput
+
+cd ~/Pulpit/corsika-77100/src/utils/coast/CorsikaRead 			# command to run CorsikaReader 
+inpCorsika=$(./CorsikaReader binaryOutput)
+
+#cd ~/Pulpit
+
+touch CorsikaReadOutput.txt
+echo "$outputCorsika" > CorsikaReadOutput.txt
+
+echo "Enter name of file which will be read:"
+read inputFile
+
 i=0
-input="./outputCorsikaInclined.txt"
+input=$inputFile
 patternid='[0-9]+'
-patternX='x=([-+][0-9]+\.?[0-9]*)|x=([0-9]*\.[0-9]+)|x=([0-9]+)'
-patternY='y=([-+][0-9]+\.?[0-9]*)|y=([0-9]*\.[0-9]+)|y=([0-9]+)'
-patternE='e=*.[0-9]{0,7}.[0-9]{0,3}*'
-patternW='w=([-+][0-9]+\.?[0-9]*)|w=([0-9]*\.[0-9]+)|w=([0-9]+)'
+patternX='x=([-+][0-9]+\.?[0-9]*)|x=([0-9]*\.[0-9]+)|x=([0-9]+)' 	#pattern of particle value of position X
+patternY='y=([-+][0-9]+\.?[0-9]*)|y=([0-9]*\.[0-9]+)|y=([0-9]+)' 	#pattern of particle value of position y
+patternE='e=*.[0-9]{0,7}.[0-9]{0,3}*'				 	#pattern of particle value of energy 
+patternW='w=([-+][0-9]+\.?[0-9]*)|w=([0-9]*\.[0-9]+)|w=([0-9]+)' 	#pattern of particle value of W
 
-touch filterXY.txt
-while IFS= read -r line
+echo "Enter name of file in which data will be saved:"
+read writeFileData
+touch $writeFileData
+
+echo "Enter position X of particle greater than:"   
+read gtX								# upper limit position X of particle
+echo "Enter position X of particle less than:"
+read ltX								# lower limit position X of particle
+echo "Enter position Y of particle greater than:"
+read gtY								# upper limit position Y of particle
+echo "Enter position Y of particle less than:"
+read ltY								# lower limit position Y of particle
+
+############################################################################################################
+
+while IFS= read -r line							# begining of reading loop 
 do  
-[[ $line =~ $patternX ]]
-a=${BASH_REMATCH[0]}
-j="10000.0"
-k="-10000.0"
-greaterThanX=$(echo "$a < $j" | bc) 
-lessThanX=$(echo "$a > $k" | bc)
-if [[ $greaterThanX = '1' ]] && [[ $lessThanX = '1' ]]; 
+[[ $line =~ $patternX ]]                                		# pattern current particle with pattern of position X 
+lessThanX=$(echo "${BASH_REMATCH[0]} > $gtX" | bc) 			# value of pattern X are compare with user parameter 
+greaterThanX=$(echo "${BASH_REMATCH[0]} < $ltX" | bc)
+if [[ $greaterThanX = '1' ]] && [[ $lessThanX = '1' ]]; 		# if value of position X are between user limit  
 then
-	((i =i + 1))
-	[[ $line =~ $patternY ]]
-	a=${BASH_REMATCH[0]}
-	greaterThanY=$(echo "$a < $j" | bc) 
-	lessThanY=$(echo "$a > $k" | bc)
-	if [[ $greaterThanY = '1' ]] && [[ $lessThanY = '1' ]];
+	
+	[[ $line =~ $patternY ]]                			# pattern current particle with pattern of position Y  
+	lessThanY=$(echo "${BASH_REMATCH[0]} > $gtY" | bc) 		# value of pattern Y are compare with user parameter 
+	greaterThanY=$(echo "${BASH_REMATCH[0]} < $ltY" | bc)
+	if [[ $greaterThanY = '1' ]] && [[ $lessThanY = '1' ]];		# if value of position X and Y are between user parameter particle is saving to file
 	then 
-		echo $line >> filterXY.txt
+		echo $line
+		echo $line >> $writeFileData				# particle which are in middle are writing to the new file
+		((i =i + 1))
 	fi
 fi
 done < "$input" 
-echo "$i" 
+#echo "$i" 
